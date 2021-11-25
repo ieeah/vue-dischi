58fbb43e24ce46b7fe84f2c48a65024e1339fc1c
ieeah <lbnfnc90@gmail.com>
2021-11-25 19:10:56 +0100

    end git

--- src/components/Albumcard.vue
+++ src/components/Albumcard.vue
@@ -1,32 +1,47 @@
 <template>
+
+<div class="wrapper">
     <div class="album_card">
         <div class="card_img">
-            <img src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg" alt="">
+            <img :src="poster" :alt="title">
         </div>
         <div class="card_details">
-            <h3>NEW JERSEY</h3>
-            <h4>Bon Jovi</h4>
-            <h5>1988</h5>
-            <h5 class="genre">Rock</h5>
+            <h3>{{title}}</h3>
+            <h4>{{subTitle}}</h4>
+            <h5>{{year}}</h5>
+            <h5 class="genre">{{genre}}</h5>
         </div>
     </div>
+</div>
+    
 </template>
 
 <script>
 
 export default {
     name: 'Albumcard',
+    props: {
+        poster: String,
+        title: String,
+        subTitle: String,
+        year: String,
+        genre: String,
+    },
 }
 </script>
 
 <style scoped lang="scss">
 @import '@/styles/vars.scss';
+
+.wrapper {
+    padding: 10px;
+    margin-bottom: 20px;
+    width: calc(100% / 8);
+}
 .album_card {
     text-align: center;
     background-color: $secondary-color;
-    margin-bottom: 20px;
-    padding: 20px;
-    width: calc(100% / 8);
+    padding: 10px;
     height: 100%;
     .card_img {
         margin-bottom: 20px;
--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,24 +1,43 @@
 <template>
     <div class="albums_container">
-        <Albumcard />
+        <Albumcard v-for="(album, i) in storedAlbums" :key="`album_${i}`"
+            :poster="album.poster" :title="album.title" :subTitle="album.author" :year="album.year" :genre="album.genre"
+        />
     </div>
 </template>
 
 <script>
 import Albumcard from '@/components/Albumcard.vue';
+import axios from 'axios';
 export default {
     name: 'Albums',
     components: {
         Albumcard,
     },
+    data() {
+        return {
+            storedAlbums: null,
+        };
+    },
+    created() {
+        this.getAlbums();
+    },
+    methods: {
+        getAlbums() {
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music').then( response => {
+                this.storedAlbums = response.data.response;
+            });
+        },
+    },
 }
 </script>
-
 <style scoped lang="scss">
 
 .albums_container {
     display: flex;
-    padding: 120px 80px;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding: 110px 70px;
 }
 
 </style>
