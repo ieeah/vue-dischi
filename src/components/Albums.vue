e35ffb1d36a09494c76f48cac682d19542ef3ebe
ieeah <lbnfnc90@gmail.com>
2021-12-02 19:24:00 +0100

    indentazione

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,7 +1,13 @@
 <template>
 	<div v-if="albums.length > 0" class="albums_container">
-		<Albumcard v-for="(album, i) in albums" :key="`album_${i}`"
-			:poster="album.poster" :title="album.title" :subTitle="album.author" :year="album.year" :genre="album.genre"
+		<Albumcard
+			v-for="(album, i) in albums" 
+			:key="`album_${i}`"
+			:poster="album.poster"
+			:title="album.title"
+			:subTitle="album.author"
+			:year="album.year"
+			:genre="album.genre"
 		/>
 	</div>
 	<Loader v-else-if="albums.length < 1" />
